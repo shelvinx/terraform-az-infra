5b8281ec0404bdd9849c56f9db366ce0692f569e
shelvinx <shelvinx@outlook.com>
2025-04-28 01:05:24 +0100

    feat: install chocolatey

--- scripts/vm-config.ps1
+++ scripts/vm-config.ps1
@@ -137,7 +137,7 @@ foreach ($setting in $registrySettings) {
 
 # Install Chocolatey
 try {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     Write-Output "Chocolatey installed successfully."
 }
 catch {
